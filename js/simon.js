fae393107dfe9449c73a5143592e269d7db9cd17
Dai Phung <dai@daiphung.com>
2017-11-07 14:36:15 +1100

    Add slider functions

--- js/simon.js
+++ js/simon.js
@@ -1,3 +1,30 @@
-function test{
+var rangeValues =
+{
+    "1": "PLEASE DON'T HURT ME...",
+    "2": "I'M READY FOR YOU!",
+    "3": "COME GET SOME!!",
+    "4": "RESISTANCE IS FUTILE!!!"
+};
+
+let rangeText = document.getElementById("rangeText");
+rangeText.innerHTML = rangeValues[1]; /* Set default slider text Value */
+$("#mid").hide(); 
+
+/* Slider on Input */
+slideInput.oninput = () => {
+    rangeText.innerHTML = rangeValues[slideInput.value];
+
+
+    if(slideInput.value === "1"){
+        $("#mid").hide('slow'); 
+        $("#content").show('slow');
+    }
     
+    if(slideInput.value === "2"){
+        $("#content").hide('slow'); 
+        $("#mid").show('slow'); 
+    }
 }
+
+
+
