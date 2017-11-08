81126c2c16a4072f64ec6609c9757cb7541bcdac
Dai Phung <dai@daiphung.com>
2017-11-09 08:47:17 +1100

    Add harder section

--- js/simon.js
+++ js/simon.js
@@ -1,14 +1,15 @@
 var rangeValues =
 {
     "1": "PLEASE DON'T HURT ME...",
-    "2": "I'M READY FOR YOU!",
-    "3": "COME GET SOME!!",
-    "4": "RESISTANCE IS FUTILE!!!"
+    "2": "I'M READY FOR YOU!!",
+    "3": "COME GET SOME!!!"
+    // "4": "RESISTANCE IS FUTILE!!!"
 };
 
 let rangeText = document.getElementById("rangeText");
 rangeText.innerHTML = rangeValues[1]; /* Set default slider text Value */
 $("#mid").hide(); 
+$("#harder").hide(); 
 
 /* Slider on Input */
 slideInput.oninput = () => {
@@ -17,12 +18,19 @@ slideInput.oninput = () => {
 
     if(slideInput.value === "1"){
         $("#mid").hide('slow'); 
+        $("#harder").hide('slow'); 
         $("#content").show('slow');
     }
     
     if(slideInput.value === "2"){
         $("#content").hide('slow'); 
+        $("#harder").hide('slow'); 
         $("#mid").show('slow'); 
+    }
+    if(slideInput.value === "3"){
+        $("#content").hide('slow'); 
+        $("#mid").hide('slow');
+        $("#harder").show('slow'); 
     }
 }
 
